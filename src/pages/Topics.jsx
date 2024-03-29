ac6f6ab366853f2beb43cd6191f79349ef05abc6
Aryan Kadam <aryankadam071@gmail.com>
2024-03-29 22:56:19 +0530

    first commit

--- src/pages/Topics.jsx
+++ src/pages/Topics.jsx
@@ -10,24 +10,49 @@ const TopicLink =
 
 const CommonContent = () => {
   return (
-    <div className="bg-yellow-800 min-h-screen">
+    <div className="bg-violet-200 text-violet min-h-screen">
       <Link to="/" className="block mb-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-3 py-2 px-4 rounded">
           Back
         </button>
       </Link>
 
-      <h1 className="text-center">Topics</h1>
+      <h1 className="text-center text-5xl font-bold mb-4">Topics</h1>
       <div className={Container}>
-        <Link to="/topics/javascript" className={TopicLink}>
-          <h4>Javascript</h4>
-        </Link>
-        <Link to="/topics/python" className={TopicLink}>
-          <h4>Python</h4>
-        </Link>
-        <Link to="/topics/css" className={TopicLink}>
-          <h4>Css</h4>
-        </Link>
+        <div className="flex justify-center">
+          <div className="flex flex-col m-4 text-center w-60">
+            <div className="bg-white shadow-md rounded-lg p-4">
+              <Link to="/topics/javascript" className={TopicLink}>
+                <h4>Javascript</h4>
+              </Link>
+            </div>
+            <div className="bg-white shadow-md rounded-lg p-4 mt-4">
+          
+            </div>
+          </div>
+
+          <div className="flex flex-col m-4 text-center w-60">
+            <div className="bg-white shadow-md rounded-lg p-4">
+              <Link to="/topics/python" className={TopicLink}>
+                <h4>Python</h4>
+              </Link>
+            </div>
+            <div className="bg-white shadow-md rounded-lg p-4 mt-4">
+              {/* Content for card 4 */}
+            </div>
+          </div>
+
+          <div className="flex flex-col m-4 text-center w-60">
+            <div className="bg-white shadow-md rounded-lg p-4">
+            <Link to="/topics/css" className={TopicLink}>
+                <h4>Css</h4>
+              </Link>
+            </div>
+            <div className="bg-white shadow-md rounded-lg p-4 mt-4">
+              {/* Content for card 6 */}
+            </div>
+          </div>
+        </div>
       </div>
       <Outlet />
     </div>
