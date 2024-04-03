0e1c9ac604717264bb5756e9d70987b74a7c29da
Aryan Kadam <aryankadam071@gmail.com>
2024-04-03 22:47:40 +0530

    Add some changes

--- src/components/CssQue.jsx
+++ src/components/CssQue.jsx
@@ -2,16 +2,22 @@ import React from "react";
 
 const ShowcssQue = ({ question, options, answerQuestion }) => {
   return (
-    <div>
-      <p>{question}</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="max-w-lg p-6 bg-violet-200 rounded-lg shadow-lg"> 
+      <p className="text-xl font-bold mb-4">{question}</p>
       <ul>
         {options.map((option, index) => (
-          <li key={index} onClick={() => answerQuestion(index)}>
+          <li
+            key={index}
+            className="cursor-pointer bg-white rounded-lg shadow-md p-2 mb-2 hover:bg-gray-300"
+            onClick={() => answerQuestion(index)}
+          >
             {option}
           </li>
         ))}
       </ul>
     </div>
+  </div>
   );
 };
 
--- src/pages/Css.jsx
+++ src/pages/Css.jsx
@@ -3,12 +3,6 @@ import ShowcssQue from '../components/CssQue';
 import { cssQuestions } from '../data/Ques';
 import { Link } from 'react-router-dom';
 
-const Container = 'mt-8';
-const AnswerCounter = 'text-2xl font-bold text-right px-4';
-const QuestionContainer = 'my-6 px-2';
-const FinalScoreContainer = 'mt-24 mx-auto w-1/2 text-center';
-const FinalScoreText = 'text-4xl';
-const FinalScoreCounter = 'text-7xl font-bold';
 
 const Css = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -30,32 +24,40 @@ const Css = () => {
   console.log(questionAttemptVerdictArray);
 
   return (
-    <>
-      <Link to="/topics">
-        <button className="py-2 px-4 mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded">Back</button>
-      </Link>
-      <div className={Container}>
+    <div className="container"> 
+    <Link to="/topics">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Back
+        </button>
+    </Link>
+    <div>
         {currentQuestionIndex < cssQuestions.length ? (
-          <>
-            <p className={AnswerCounter}>
-              Correct Answers: <span data-testid="quiz-answer-counter">{correctAnswers}</span> / {cssQuestions.length}
-            </p>
+            <>
+                <p className="text-2xl font-bold text-right px-4">
+                    Correct Answers: <span>{correctAnswers}</span> /{' '}
+                    {cssQuestions.length}
+                </p>
 
-            <div className={QuestionContainer}>
-              <p>Question: {currentQuestionIndex + 1}</p>
-              <ShowcssQue {...cssQuestions[currentQuestionIndex]} answerQuestion={answerQuestion} />
-            </div>
-          </>
+                <div>
+                    <p className="text-2xl font-bold">
+                        Question: {currentQuestionIndex + 1}
+                    </p>
+                    <ShowcssQue
+                        {...cssQuestions[currentQuestionIndex]}
+                        answerQuestion={answerQuestion}
+                    />
+                </div>
+            </>
         ) : (
-          <div className={FinalScoreContainer}>
-            <h3 className={FinalScoreText}>Final Score</h3>
-            <p className={FinalScoreCounter}>
-              <span data-testid="final-answer-counter">{correctAnswers}</span> / {cssQuestions.length}
-            </p>
-          </div>
+            <div className="mt-36 text-center">
+                <h3 className="text-2xl font-bold">Final Score</h3>
+                <p className="text-6xl font-bold">
+                    <span>{correctAnswers}</span> / {cssQuestions.length}
+                </p>
+            </div>
         )}
-      </div>
-    </>
+    </div>
+</div>
   );
 };
 
