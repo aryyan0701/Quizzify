0890afc22f8b8c9f2e178dcf5980d2bb7a16327e
Aryan kadam <113440600+aryyan0701@users.noreply.github.com>
2024-07-28 22:22:38 +0530

    Add some changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+
+
+  return (
+    <>
+    <div className="bg-blue-950 p-4 flex justify-between items-center">
+      <div className="text-4xl font-bold text-white">Quizzify</div>
+      <div className="flex justify-between items-center">
+        <input
+          className="rounded hidden lg:block border border-gray-600 py-1 px-2 me-4"
+          type="text"
+          placeholder="Search"
+        />
+        <FontAwesomeIcon className="text-2xl me-3 cursor-pointer text-white" icon={faMagnifyingGlass} />
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Navbar;
